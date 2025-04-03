b6ca73d14c5eaccb23cd856b12e61364dd4786e7
MalikUmais <umaisfaruq03@gmail.com>
2025-04-04 00:29:56 +0500

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
 import Container from "./container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-export { Header, Footer,Container,Logo, LogoutBtn };
+import PostCard from "./PostCard"
+import Select from "./Select";
+export { Header, Footer,Container,Logo, LogoutBtn,Button, Input, Login, PostCard, Select };
