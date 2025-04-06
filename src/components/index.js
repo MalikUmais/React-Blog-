d3bbdaae9bd5429f0f37633e3d27a53be4e9ec3a
MalikUmais <umaisfaruq03@gmail.com>
2025-04-06 17:52:52 +0500

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 import Container from "./container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import { Protected } from './AuthLayout'
 import Button from "./Button";
 import Input from "./Input";
 import Login from "./Login";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import PostCard from "./PostCard"
+import RTE from "./RTE";
 import Select from "./Select";
-export { Header, Footer,Container,Logo, LogoutBtn,Button, Input, Login, PostCard, Select };
+import Signup from "./Signup";
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Login, PostCard, Select, RTE, Signup, Protected };
