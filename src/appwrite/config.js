2034d5433702c12ef05ed78c7995c112ce1ec113
MalikUmais <umaisfaruq03@gmail.com>
2025-04-08 01:05:06 +0500

    finalizing

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,150 +1,3 @@
-// import conf from "../conf/conf";
-// import { Client, ID, Databases, Storage, Query } from "appwrite";
-
-// export class Service {
-//     client = new Client();
-//     databases;
-//     bucket;
-
-//     constructor() {
-//         this.client
-//             .setEndpoint(conf.appwriteUrl)
-//             .setProject(conf.appwriteProjectId);
-//         this.databases = new Databases(this.client);
-//         this.bucket = new Storage(this.client);
-//     }
-
-//     // Method to create a post
-//     async createPost({ title, slug, content, featuredImage, status, userId }) {
-//         try {
-//             return await this.databases.createDocument(
-//                 conf.appwriteDatabaseId, // Changed from appwriteDtabaseId
-//                 conf.appwriteCollectionId,
-//                 slug,
-//                 {
-//                     title,
-//                     content,
-//                     featuredImage,
-//                     status,
-//                     userId,
-//                 }
-//             );
-//         } catch (err) {
-//             console.error("Create post error:", err);
-//             throw err;
-//         }
-//     }
-
-//     // Method to update post
-//     async updatePost(slug, { title, content, featuredImage, status }) {
-//         try {
-//             return await this.databases.updateDocument(
-//                 conf.appwriteDatabaseId, // Changed from appwriteDtabaseId
-//                 conf.appwriteCollectionId,
-//                 slug,
-//                 {
-//                     title,
-//                     content,
-//                     featuredImage,
-//                     status,
-//                 }
-//             );
-//         } catch (err) {
-//             console.error("Update post error:", err);
-//             throw err;
-//         }
-//     }
-
-//     // Method to delete post
-//     async deletePost(slug) {
-//         try {
-//             await this.databases.deleteDocument(
-//                 conf.appwriteDatabaseId, // Changed from appwriteDtabaseId
-//                 conf.appwriteCollectionId,
-//                 slug,
-//             );
-//             return true;
-//         } catch (err) {
-//             console.error("Delete post error:", err);
-//             return false;
-//         }
-//     }
-
-//     // Method to get post
-//     async getPost(slug) {
-//         try {
-//             return await this.databases.getDocument(
-//                 conf.appwriteDatabaseId, // Changed from appwriteDtabaseId
-//                 conf.appwriteCollectionId,
-//                 slug,
-//             );
-//         } catch (err) {
-//             console.error("Get post error:", err);
-//             throw err;
-//         }
-//     }
-
-//     // Method to get all posts
-//     async getPosts(queries = [Query.equal('status', 'active')]) {
-//         try {
-//             return await this.databases.listDocuments(
-//                 conf.appwriteDatabaseId, // Changed from appwriteDtabaseId
-//                 conf.appwriteCollectionId,
-//                 queries,
-//             );
-//         } catch (err) {
-//             console.error("Get posts error:", err);
-//             throw err;
-//         }
-//     }
-
-//     // File upload service
-//     // Method to upload file
-//     async uploadFile(file) {
-//         try {
-//             return await this.bucket.createFile(
-//                 conf.appwriteBucketId,
-//                 ID.unique(),
-//                 file,
-//             );
-//         } catch (err) {
-//             console.error("Upload file error:", err);
-//             return false;
-//         }
-//     }
-
-//     // Method to delete file
-//     async deleteFile(fileId) {
-//         try {
-//             await this.bucket.deleteFile(
-//                 conf.appwriteBucketId,
-//                 fileId,
-//             );
-//             return true;
-//         } catch (err) {
-//             console.error("Delete file error:", err);
-//             return false;
-//         }
-//     }
-
-//     // Method to get file preview
-//     async getFilePreview(fileId) {
-//         try {
-//             return this.bucket.getFilePreview(
-//                 conf.appwriteBucketId,
-//                 fileId,
-//             );
-//         } catch (err) {
-//             console.error("Get file preview error:", err);
-//             throw err;
-//         }
-//     }
-// }
-
-// const service = new Service();
-// export default service;
-
-
 import conf from "../conf/conf";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
@@ -267,14 +120,37 @@ export class Service {
         }
     }
 
+    // getFilePreview(fileId) {
+    //     try {
+    //         return this.bucket.getFilePreview(
+    //             conf.appwriteBucketId,
+    //             fileId
+    //         ).href;
+    //     } catch (error) {
+    //         console.error("Appwrite service :: getFilePreview :: error", error);
+    //         return null;
+    //     }
+    // }
+
     getFilePreview(fileId) {
         try {
             return this.bucket.getFilePreview(
                 conf.appwriteBucketId,
                 fileId
-            ).href;
+            );
         } catch (error) {
             console.error("Appwrite service :: getFilePreview :: error", error);
+            return null;
+        }
+    }
+    getFileView(fileId){
+        try {
+            return this.bucket.getFileView(
+                conf.appwriteBucketId,
+                fileId
+            );
+        } catch (error) {
+            console.error("Appwrite service :: getFileView :: error", error);
             return null;
         }
     }
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import service from "../appwrite/config";
-// import { Link } from "react-router-dom";
-
-// function PostCard({ $id, title, featuredImage }) {
-//   return (
-//     <Link to={`/post/${$id}`}>
-//       <div className="w-full bg-gray-100 rounded-xl p-4">
-//         <div className="w-full justify-center mb-4">
-//           <img
-//             src={service.getFilePreview(featuredImage)}
-//             alt={title}
-//             className="rounded-xl "
-//           />
-//         </div>
-//         <h2 className="text-xl font-bold">{title}</h2>
-//       </div>
-//     </Link>
-//   );
-// }
-
-// export default PostCard;
-
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 import service from "../appwrite/config";
@@ -32,13 +7,13 @@ function PostCard({ $id, title, featuredImage }) {
     try {
       if (!imageId) return null;
       console.log(imageId);
-      console.log (service.getFilePreview(imageId));
-      return service.getFilePreview(imageId);
+      return service.getFileView(imageId);
     } catch (error) {
       console.error("Error getting image URL:", error);
       return null;
     }
   };
+  
 
   return (
     <Link to={`/post/${$id}`}>
@@ -50,7 +25,12 @@ function PostCard({ $id, title, featuredImage }) {
               alt={title}
               className="rounded-xl w-full h-48 object-cover"
               onError={(e) => {
-                e.target.src = 'https://via.placeholder.com/300x200?text=No+Image';
+                const fallback =
+                  "https://dummyimage.com/300x200/cccccc/000000&text=No+Image";
+                if (!e.target.dataset.fallbackUsed) {
+                  e.target.src = fallback;
+                  e.target.dataset.fallbackUsed = "true";
+                }
               }}
             />
           )}
